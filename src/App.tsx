4db22b37458d6366f488edba73a878ea2205bbc1
taeyong.kwon <taeyong.kwon@jalsalda.com>
2021-01-25 22:37:13 +0900

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class TodoApp extends React.Component {
+
+  render(){
+    return (
+      <div className="App">
+        <div className="cardHeader">
+          <input id="newTodo"className="todo"/>
+          <button id="addTodo"></button>
+        </div>
+        <div className="cardBody">
+
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default TodoApp;
